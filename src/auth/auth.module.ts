4e64fb62b5a9a54b9081d888f6b8d37daf3406b4
meselghea <122159476+meselghea@users.noreply.github.com>
2023-05-13 13:59:59 +0700

    change sign expired session 1d, change dto password match with regrex

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -16,7 +16,7 @@ export const jwtSecret = process.env.SECRET_KEY;
     PassportModule,
     JwtModule.register({
       secret: process.env.SECRET_KEY,
-      signOptions: { expiresIn: '30m' }, 
+      signOptions: { expiresIn: '1d' }, 
     }),
     UsersModule,
   ],
